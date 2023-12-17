2b97e56959cea3a866713c4849924171090ad761
Mhd Basel Bashir <u13472@isoint.net>
2023-12-17 23:27:58 +0300

    update service

--- src/main/java/microservices/orderservice/Controllers/OrderController.java
+++ src/main/java/microservices/orderservice/Controllers/OrderController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import microservices.orderservice.DTOs.Requests.OrderRequest;
 import microservices.orderservice.Services.OrderService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -14,10 +15,13 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/placeOrder")
-    @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order placed successfully";
+    public ResponseEntity<Object> placeOrder(@RequestBody OrderRequest orderRequest) {
+        try {
+            orderService.placeOrder(orderRequest);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Order placed successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.FAILED_DEPENDENCY).body(e.getMessage());
+        }
     }
 }
 
